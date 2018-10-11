fd388cab091db53eae0574846565d60b82eb9cf5
csababalint91 <40868013+csababalint91@users.noreply.github.com>
2018-10-11 16:04:52 +0100

    Add countdown button

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function revealMessage() {
 	document.getElementById("hiddenMessage").style.display = 'block';
 }
+
+function countDown() {
+	var currentVal = document.getElementById("countDownButton").innerHTML;
+	var newVal = currentVal - 1;
+	document.getElementById('countDownButton').innerHTML = newVal;
+}
